c9f7118e281bd82b263f9445cc5306ac1a73a083
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-14 09:24:26 +0100

    Adds auth and redirect with jwt

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 [![Build Status](https://travis-ci.org/telemark/micro-auth-ldap.svg?branch=master)](https://travis-ci.org/telemark/micro-auth-ldap)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+[![Greenkeeper badge](https://badges.greenkeeper.io/telemark/micro-auth-ldap.svg)](https://greenkeeper.io/)
 
 # micro-auth-ldap
 
-[![Greenkeeper badge](https://badges.greenkeeper.io/telemark/micro-auth-ldap.svg)](https://greenkeeper.io/)
 ldap auth microservice
 
 ## License
+
 [MIT](LICENSE)
 
 ![alt text](https://robots.kebabstudios.party/micro-auth-ldap.png "Robohash image of micro-auth-ldap")
--- index.js
+++ index.js
@@ -8,17 +8,27 @@ const bodyParser = require('urlencoded-body-parser')
 const loginPage = require('./lib/render-login-page')
 const lookupUser = require('./lib/lookup-user')
 const saveSession = require('./lib/save-session')
+const loginUser = require('./lib/login-user')
+const generateJwt = require('./lib/generate-jwt')
 
 module.exports = async (request, response) => {
   const {pathname, query} = await parse(request.url, true)
   if (pathname === '/auth') {
     const data = request.method === 'POST' ? await bodyParser(request) : query
-    send(response, 200, data)
+    const result = await loginUser(data)
+    const session = await saveSession(result)
+    const jwt = generateJwt(Object.assign({sessionKey: session}, result))
+    const url = `${data.redirectUrl}?jwt=${jwt}`
+    response.writeHead(301,  { Location: url })
+    response.end()
   } else if (pathname === '/lookup') {
     const data = request.method === 'POST' ? await json(request) : query
     const result = await lookupUser(data)
     const session = await saveSession(result)
-    send(response, 200, session)
+    const jwt = generateJwt(Object.assign({sessionKey: session}, result))
+    const url = `${data.redirectUrl}?jwt=${jwt}`
+    response.writeHead(301,  { Location: url })
+    response.end()
   } else if (pathname === '/login') {
     const data = request.method === 'POST' ? await json(request) : query
     send(response, 200, loginPage(data))
