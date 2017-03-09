0d37c570fa898b3315d5275185d68b687d29136b
maccyber <jonas.enge@gmail.com>
2017-03-09 12:27:39 +0100

    Fixes error url

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ module.exports = async (request, response) => {
     } catch (error) {
       console.log(error)
       const errorMessage = typeof error === 'string' ? error : error.message || 'Unknown error'
-      response.writeHead(302, { Location: `/login?origin=${data.origin}&nextPath=${query.nextPath || ''}&error=${errorMessage}` })
+      const url = `/login?origin=${data.origin}&nextPath=${query.nextPath || ''}&error=${errorMessage}`
+      console.log(url)
+      response.writeHead(302, { Location: url })
       response.end()
     }
   } else if (query.jwt) {
